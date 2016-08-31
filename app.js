b92c99273222cdcb09629554b7d2f552599e8688
Shep Moore-Berg <mooreberg@g.harvard.edu>
2016-08-30 22:15:27 -0400

    Added Bot Emulator Framework. Also updated dialog to include question and answer.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
   
 // Create chat bot
+//var connector = new builder.ConsoleConnector().listen();
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -23,6 +24,24 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function(session) {
-    session.send('Hello to you!');
-});
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.choice(session, "Hi " + results.response + ". Who is your favorite Mask and Wig alum?",
+        ["Shep Moore-Berg", "Adam Savitt", "Jeff Walton"]); 
+    },
+    function (session, results) {
+        session.userData.favorite = results.response.entity;
+        builder.Prompts.choice(session, "Okay, now why is he your favorite? Because you ...", 
+        ["liked him in his sophomore year show.", "think he looks great dressed as a princess.", "are married to him."]);
+    },
+    function (session, results) {
+        session.userData.reason = results.response.entity;
+        session.send("Got it " + session.userData.name + 
+                     "! You think " + session.userData.favorite + " is awesome because you " + session.userData.reason
+                     + " That's pretty cool.");
+    }
+]);
